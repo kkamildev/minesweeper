82159dc48aaca3c29d48076e9f7b3873cbaeccbb
kkamildev <kamil.kijak@pixlesofte.com>
2026-02-05 23:03:15 +0100

    added handling mouse click and started creating bomb planting

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 
 #include "raylib.h"
 #include "iostream"
+#include "stdlib.h"
+#include "time.h"
 
 
 const int WINDOW_WIDTH = 1080;
@@ -10,6 +12,7 @@ const int MINEFIELD_WIDTH = 18;
 const int MINEFIELD_HEIGHT = 14;
 
 const int CELL_SIZE = 40;
+const int BOMB_COUNT = 40;
 
 char mineField[MINEFIELD_HEIGHT][MINEFIELD_WIDTH];
 bool checkedFields[MINEFIELD_HEIGHT][MINEFIELD_WIDTH];
@@ -51,18 +54,44 @@ void PrepareFieldForGame(char field[MINEFIELD_HEIGHT][MINEFIELD_WIDTH], bool che
     }
 }
 
-void DrawBoard(char field[MINEFIELD_HEIGHT][MINEFIELD_WIDTH], bool checkedField[MINEFIELD_HEIGHT][MINEFIELD_WIDTH], Vector2 primaryPosition) {
+void DrawBoard(char field[MINEFIELD_HEIGHT][MINEFIELD_WIDTH], bool checkedField[MINEFIELD_HEIGHT][MINEFIELD_WIDTH], Vector2 primaryPosition, Vector2 selectedField) {
     for(int i = 0;i<MINEFIELD_HEIGHT;i++) {
         for(int j = 0;j<MINEFIELD_WIDTH;j++) {
+            if((int)selectedField.x == j && (int)selectedField.y == i) {
+                DrawRectangle(CELL_SIZE * j + primaryPosition.x, CELL_SIZE * i + primaryPosition.y, CELL_SIZE, CELL_SIZE, GRAY);
+            }
             DrawRectangleLines(CELL_SIZE * j + primaryPosition.x, CELL_SIZE * i + primaryPosition.y, CELL_SIZE, CELL_SIZE, BLACK);
         }
     }
     DrawRectangleLinesEx((Rectangle){primaryPosition.x, primaryPosition.y, CELL_SIZE * MINEFIELD_WIDTH, CELL_SIZE * MINEFIELD_HEIGHT}, 4, BLACK);
 }
 
+Vector2 GetField(Vector2 mousePosition, Vector2 primaryPosition) {
+    return {(mousePosition.x - primaryPosition.x) / CELL_SIZE, (mousePosition.y - primaryPosition.y) / CELL_SIZE};
+}
+
+void PlantBombs(char field[MINEFIELD_HEIGHT][MINEFIELD_WIDTH], int bombs, Vector2 selectedField) {
+    srand(time(0));
+    Vector2 bombPosition;
+    while(bombs > 0) {
+        bombPosition = {(float)(rand() % MINEFIELD_WIDTH), (float)(rand() % MINEFIELD_HEIGHT)};
+        if(selectedField.x == bombPosition.x && selectedField.y == bombPosition.y) {
+            continue;
+        } else {
+            // place bomb
+            field[(int)bombPosition.y][(int)bombPosition.x] = 'B';
+            bombs--;
+        }
+    }
+}
+
 
 
 int main() {
+    Vector2 mousePosition;
+    Vector2 primaryGridPosition = {50, 100};
+    Vector2 selectedField;
+    bool started = false;
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "MINESWEEPER");
     
     // loading assets
@@ -70,12 +99,32 @@ int main() {
 
     // loading content
     Text title("MINESWEEPER", {WINDOW_WIDTH / 2, 0}, 80);
+
+    int bombCount = BOMB_COUNT;
+
     PrepareFieldForGame(mineField, checkedFields);
     while(!WindowShouldClose()) {
+
+        // mouse logic
+        mousePosition = GetMousePosition();
+        selectedField = {-1, -1};
+        if(mousePosition.x >= primaryGridPosition.x && mousePosition.y >= primaryGridPosition.y) {
+            if(mousePosition.x <= primaryGridPosition.x + CELL_SIZE * MINEFIELD_WIDTH && mousePosition.y <= primaryGridPosition.y + CELL_SIZE * MINEFIELD_HEIGHT) {
+                selectedField = GetField(mousePosition, primaryGridPosition); 
+            }
+        }
+        if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            if(selectedField.x > -1) {
+                if(!started) {
+                    // setting all bombs
+                    PlantBombs(mineField, bombCount, selectedField);
+                }
+            }
+        }
         BeginDrawing();
         ClearBackground(RAYWHITE);
         title.DrawCentered();
-        DrawBoard(mineField, checkedFields, (Vector2){50, 100});
+        DrawBoard(mineField, checkedFields, primaryGridPosition, selectedField);
         EndDrawing();
     }
     CloseWindow();
